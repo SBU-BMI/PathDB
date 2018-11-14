69e1ad493c1e3d6bf03494049c2cbc16a552a865
Erich Bremer <erich@ebremer.com>
2018-11-14 10:28:15 -0500

    Add initial menu items

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 if [ ! -d /data/mysql ]; then
-        echo "PathDB not initialized!  Initializing..."
-        echo "PathDB not initialized!  Initializing..." > elog
-        #mkdir /data/mysql
-        #chown -R mysql /data/mysql
-        #service mariadb start
+
+# PathDB not initialized.  Create default MySQL database and make PathDB changes
+
         mysql_install_db --user=mysql --ldata=/data/mysql
         /usr/bin/mysqld_safe --datadir='/data/mysql' &
         sleep 10
@@ -25,8 +23,14 @@ if [ ! -d /data/mysql ]; then
 	/data/quip/vendor/bin/drush -y cache-rebuild
 	httpd -f /etc/httpd/conf/httpd.conf
 	sleep 2
+
+# create private and public security taxonomy items
 	curl --user admin:bluecheese2018 -k -X POST http://localhost/taxonomy/term?_format=json -H "Content-Type: application/json" -d '{"vid": [{"target_id": "collections","target_type": "taxonomy_vocabulary"}],"name": [{"value": "Private"}]}'
 	curl --user admin:bluecheese2018 -k -X POST https://localhost/taxonomy/term?_format=json -H "Content-Type: application/json" -d '{"vid": [{"target_id": "collections","target_type": "taxonomy_vocabulary"}],"name": [{"value": "Public"}]}'
+# create PathDB menu items
+	curl --user admin:bluecheese2018 -k -X POST https://localhost/entity/menu_link_content?_format=json -H "Content-Type: application/json" -d '{"bundle": [{"value": "menu_link_content"}],"enabled": [{"value": true}],"title": [{"value": "Get a JWT!"}],"description": [],"menu_name": [{"value": "main"}],"link": [{"uri": "internal:/jwt/token","title": "","options": []}],"weight": [{"value": 3}],"expanded": [{"value": true}]}'
+        curl --user admin:bluecheese2018 -k -X POST https://localhost/entity/menu_link_content?_format=json -H "Content-Type: application/json" -d '{"bundle": [{"value": "menu_link_content"}],"enabled": [{"value": true}],"title": [{"value": "Images"}],"description": [],"menu_name": [{"value": "main"}],"link": [{"uri": "internal:/listofimages","title": "","options": []}],"weight": [{"value": 2}],"expanded": [{"value": true}]}'
+	curl --user admin:bluecheese2018 -k -X POST https://localhost/entity/menu_link_content?_format=json -H "Content-Type: application/json" -d '{"bundle": [{"value": "menu_link_content"}],"enabled": [{"value": true}],"title": [{"value": "Collections"}],"description": [],"menu_name": [{"value": "main"}],"link": [{"uri": "internal:/collections","title": "","options": []}],"weight": [{"value": 1}],"expanded": [{"value": true}]}'
 else
 	httpd -f /etc/httpd/conf/httpd.conf
 fi
