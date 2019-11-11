211bbc4d1ee598ca5b8c8d70bc7e8a3ad62ed993
ebremer <erich@ebremer.com>
2019-11-11 10:07:25 -0500

    add header modification library

---
+++ quip/web/modules/contrib/http_response_headers/src/Controller/ResponseHeaderListBuilder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Drupal\http_response_headers\Controller;
+
+use Drupal\Core\Config\Entity\ConfigEntityListBuilder;
+use Drupal\Core\Entity\EntityInterface;
+
+/**
+ * Provides a listing of Response Headers.
+ */
+class ResponseHeaderListBuilder extends ConfigEntityListBuilder {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildHeader() {
+    $header['label'] = $this->t('Example');
+    $header['id'] = $this->t('Machine name');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    $row['label'] = $this->getLabel($entity);
+    $row['id'] = $entity->id();
+
+    return $row + parent::buildRow($entity);
+  }
+
+}
